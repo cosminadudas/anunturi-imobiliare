da627d460d1b1dfc19f75de4ac0d7ee1794d2762
cosminadudas <cosminadudas@users.noreply.github.com>
2020-06-28 17:16:27 +0300

    added more functionalities to templates

--- django_project/anunt/views.py
+++ django_project/anunt/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+anunturi = [
+    {
+        'autor' : 'Cosmina',
+        'titlu' : 'Anunt apartament 1',
+        'descriere' : 'Primul apartament de vanzare',
+        'data_postarii' : 'Iunie 28, 2020'
+    },
+    {
+        'autor' : 'David',
+        'titlu' : 'Anunt apartament 2',
+        'descriere' : 'Al doilea apartament de vanzare',
+        'data_postarii' : 'Iunie 29, 2020'
+    }
+]
+
+
 def home(request):
-    return render(request, 'anunt/home.html')
+    context = {
+        'anunturi': anunturi
+    }
+    return render(request, 'anunt/home.html', context)
 
 def about(request):
-    return render(request, 'anunt/about.html')
+    return render(request, 'anunt/about.html', {'title': 'About'})
