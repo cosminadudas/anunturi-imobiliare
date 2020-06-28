3f4856ca90f7729477392ec5d6848de34845bff5
cosminadudas <cosminadudas@users.noreply.github.com>
2020-06-28 17:01:48 +0300

    added more complex templates for home and about pages

--- django_project/anunt/views.py
+++ django_project/anunt/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Anunt home</h1>')
+    return render(request, 'anunt/home.html')
 
 def about(request):
-    return HttpResponse('<h1>Anunt about</h1>')
+    return render(request, 'anunt/about.html')
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'anunt.apps.AnuntConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
